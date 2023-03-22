6adff5dc66467f9ae62eca2500e5a1d77dca4a3d
heissenberg303 <88345008+heissenberg303@users.noreply.github.com>
2023-03-23 00:04:46 +0700

    fix discord.js version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord BOT
+
+## Prerequisite
+- node version >= 10.00
+- discord version >= 14.00
--- bot.js
+++ bot.js
@@ -1,21 +1,41 @@
-const Discord = require('discord.js');
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const token = process.env.DISCORD_TOKEN
+const appId = process.env.APP_ID
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(appId), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 const { Client, GatewayIntentBits } = require('discord.js');
-
-
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-]});
-
-const token = 'MTA4Nzc2MDA2OTEzMjgyNDYzOQ.GGDSYF.fbc9zcKmxzjAZz4kRTkEwdPX1d7pwgihMdbw0o';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
-  if (message.content === '!ping') {
-    message.channel.send('Pong!');
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
 });
 
